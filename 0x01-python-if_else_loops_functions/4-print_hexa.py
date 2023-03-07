160cb3e57468622e7143ecd90f14dc53db832a90
Dukeson Ehigboria O <studysampul@gmail.com>
2023-03-07 17:00:15 +0100

    If else statement in Python. Task 4

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# A python program to print numbers and their hexadecimal equivalent
+# Author - Dukeson Ehigboria
+
+for num in range(99):
+    print("{} = {}".format(num, hex(num)))
