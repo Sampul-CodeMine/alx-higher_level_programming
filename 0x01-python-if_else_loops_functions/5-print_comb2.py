a8a69ed86ec5a1dfaf093c585d7c2238bb25f994
Sampul-CodeMine <studysampul@gmail.com>
2023-03-07 21:40:46 +0100

    Updated script for task 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for digit in range(100):
-	if digit == 99:
-		print("{}".format(digit))
-	else:
-		print("{:02}".format(digit), end=", ")
+    if digit == 99:
+        print("{}".format(digit))
+    else:
+        print("{:02}".format(digit), end=", ")
