a93405c401429a7c95ffcc2354b0abee3d174f59
Sampul-CodeMine <studysampul@gmail.com>
2023-03-07 22:40:09 +0100

    Updated script for task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# function to return a string in uppercase
+
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print(f"{char}", end="")
+    print()
