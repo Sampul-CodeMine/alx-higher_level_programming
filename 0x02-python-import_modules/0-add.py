6a5c28c8a7966a9dff00256f14259d0fef07fd60
Dukeson Ehigboria O <studysampul@gmail.com>
2023-03-09 14:37:52 +0100

    Program to import the add module. Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     # import the module add_0
     from add_0 import add
 
     # variable declaration and assignment
     a = 1
     b = 2
-    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
