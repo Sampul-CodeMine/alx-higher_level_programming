febe20f228eec967b5ef5c8c402de33565826489
Sampul-CodeMine <studysampul@gmail.com>
2023-03-09 21:19:02 +0100

    Python program import modules. Update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 def add_arg(argv):
-    arg_len = len(argv) - 1
-    if arg_len == 0:
-        print("{:d}".format(arg_len))
-        return
-    else:
-        itr = 1
-        add = 0
-        while itr <= n:
-            add += int(argv[itr])
-            itr += 1
-        print("{:d}".format(add))
+    summ = 0
+    for itr in range(len(sys.argv) - 1):
+        summ += int(argv[itr + 1])
+    print("{}".format(summ))
 
 if __name__ == "__main__":
     import sys
