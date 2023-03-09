2bd6482a4c009905d63dac56908db76a9d9d3ca3
Sampul-CodeMine <studysampul@gmail.com>
2023-03-09 14:11:14 +0100

    Python program to import add module. Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,8 @@
 if __name__ == '__main__':
     # import the module add_0
     from add_0 import add
+
     # variable declaration and assignment
     a = 1
     b = 2
-    print(f"{a:d} + {b:d} = {add(a, b)}")
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
