9fadde3121577b389750e73219bf1fa48870e539
Dukeson Ehigboria O <studysampul@gmail.com>
2023-03-28 11:41:46 +0100

    Updated Code to Task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -11,11 +11,11 @@ def safe_print_integer_err(value):
 
     Returns:
         Returns True if successful, else False if a TypeError or ValueError
-    occurs 
+    occurs
     """
     try:
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (False)
+        return False
