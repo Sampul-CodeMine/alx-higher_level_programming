0188b3ce802f86a4cce4d48e46e27cd282c804ff
Dukeson Ehigboria O <studysampul@gmail.com>
2023-04-04 18:52:15 +0100

    Solution to the N-queen Board game

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+"""Program that solves the N-queens puzzle.
+Find all possible solutions to placing N.
+N non-attacking queens on an NxN chessboard.
+N must be of integer value greater than or equal to 4.
+Attributes:
+    chessboard: A list of lists representing the chessboard (list).
+    solutions: A list of lists containing solutions (list).
+"""
+import sys
+
+
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    chessboard = []
+    for i in range(n):
+        chessrow = []
+        for j in range(n):
+            chessrow.append(' ')
+        chessboard.append(chessrow)
+    return chessboard
+
+
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return board
+
+
+def get_solution(board):
+    """Return the list of lists representation of a solved chessboard."""
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return solution
+
+
+def xout(board, row, col):
+    """cross out spots on a chessboard using X.
+    All non-attacking spots where the queens can no
+    longer be played are X-ed (crossed) out.
+    Args:
+        board: The current working chessboard (list).
+        row: The row where a queen was last played (int).
+        col: The column where a queen was last played (int).
+    """
+    # X (cross) out all used spots
+    for c in range(col + 1, len(board)):
+        board[row][c] = "x"
+    # X out all backwards spots
+    for c in range(col - 1, -1, -1):
+        board[row][c] = "x"
+    # X out all spots below
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
+    # X out all spots above
+    for r in range(row - 1, -1, -1):
+        board[r][col] = "x"
+    # X out all spots diagonally down to the right
+    c = col + 1
+    for r in range(row + 1, len(board)):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # X out all spots diagonally up to the left
+    c = col - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        board[r][c]
+        c -= 1
+    # X out all spots diagonally up to the right
+    c = col + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # X out all spots diagonally down to the left
+    c = col - 1
+    for r in range(row + 1, len(board)):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -= 1
+
+
+def recursive_solve(board, row, queens, solution):
+    """Recursively solve an N-queens puzzle.
+    Args:
+        board (list): The current working chessboard.
+        row (int): The current working row.
+        queens (int): The current number of placed queens.
+        solution (list): A list of lists of solutions.
+    Returns:
+        solution
+    """
+    if queens == len(board):
+        solution.append(get_solution(board))
+        return solution
+
+    for c in range(len(board)):
+        if board[row][c] == " ":
+            tmp_board = board_deepcopy(board)
+            tmp_board[row][c] = "Q"
+            xout(tmp_board, row, c)
+            solution = recursive_solve(tmp_board, row + 1,
+                                       queens + 1, solution)
+    return solution
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    chsboard = init_board(int(sys.argv[1]))
+    solutions = recursive_solve(chsboard, 0, 0, [])
+    for sol in solutions:
+        print(sol)
