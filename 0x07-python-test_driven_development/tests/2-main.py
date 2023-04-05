031d9a39b0bff37698878c3cf5fc86d22ca2908d
Dukeson Ehigboria O <studysampul@gmail.com>
2023-04-05 19:53:12 +0100

    Updates to Task 2

--- 0x07-python-test_driven_development/tests/2-main.py
+++ 0x07-python-test_driven_development/tests/2-main.py
@@ -2,9 +2,9 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
+    [2, 3, 5,],
+    [7, 8, "9"]
 ]
-print(matrix_divided(matrix, 3))
+print(matrix_divided(matrix, 2))
 print(matrix)
 
