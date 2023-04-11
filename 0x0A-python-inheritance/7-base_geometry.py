e668bcbc45cc6e598b4768be045d9aa4f797dc9e
Dukeson Ehigboria O <studysampul@gmail.com>
2023-04-11 20:19:11 +0100

    Solution to Class Inheritanc Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a Geometry Class"""
+
+
+class BaseGeometry:
+    """A class that represents a Geometric object"""
+
+    def area(self):
+        """A public method that is not implemented"""
+        raise Exception(f"area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function to validate an integer passed as an argument
+        Args:
+            name (str): The name of the argument passed
+            value (int): The argument to validate
+        Raises:
+            TypeError: if the datatype of `value` is not integer
+            ValueError: if `value` is < or = 0 (zero)
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
