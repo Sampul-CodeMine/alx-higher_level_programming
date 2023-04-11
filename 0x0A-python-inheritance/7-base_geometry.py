02e1271db20f852676caa1e081f1040d0bae511a
Dukeson Ehigboria O <studysampul@gmail.com>
2023-04-11 20:09:46 +0100

    Solution to Class Inheritanc Task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,7 +18,7 @@ class BaseGeometry:
             TypeError: if the datatype of `value` is not integer
             ValueError: if `value` is < or = 0 (zero)
         """
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
