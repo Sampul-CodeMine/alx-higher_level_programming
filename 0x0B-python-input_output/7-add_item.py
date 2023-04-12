d28686844b1b208d6fdea3cdc6d341ce9b7c278b
Dukeson Ehigboria O <studysampul@gmail.com>
2023-04-12 18:16:09 +0100

    Solution to Task 8

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A python script that adds arguments passed from the CLI
+during program execution and save them to a file."""
+
+
+if __name__ == "__main__":
+    from sys import argv as arg
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').\
+        load_from_json_file
+
+    try:
+        vargs = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        vargs = []
+    vargs.extend(arg[1:])
+    save_to_json_file(vargs, "add_item.json")
