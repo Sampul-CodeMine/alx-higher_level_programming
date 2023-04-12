62cef17316f01fffbe8f46258d9b1691a662b3f3
Dukeson Ehigboria O <studysampul@gmail.com>
2023-04-12 18:19:53 +0100

    Solution to Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,7 @@ during program execution and save them to a file."""
 
 
 if __name__ == "__main__":
-    import sys as s
+    import sys
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').\
         load_from_json_file
@@ -13,5 +13,5 @@ if __name__ == "__main__":
         vargs = load_from_json_file("add_item.json")
     except FileNotFoundError:
         vargs = []
-    vargs.extend(s.argv[1:])
+    vargs.extend(sys.argv[1:])
     save_to_json_file(vargs, "add_item.json")
