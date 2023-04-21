742e1124538c92c4f9ea0d5a91de9e410bf08113
Dukeson Ehigboria O <studysampul@gmail.com>
2023-04-21 09:47:32 +0100

    Refactoring my codes

---
+++ 0x0C-python-almost_a_circle/server_test/test2.py
@@ -0,0 +1,14 @@
+""" Check """
+from models.base import Base
+
+rjson = Base.to_json_string([])
+
+if rjson is None:
+    print("to_json_string is not returning a string")
+    exit(1)
+
+if type(rjson) is not str:
+    print("to_json_string is not returning a string: {}".format(type(rjson)))
+    exit(1)
+
+print("OK", end="")
