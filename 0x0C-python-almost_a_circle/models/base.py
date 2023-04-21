f8b68d08f447dddad92d723b00c6c5150ab63e1c
Dukeson Ehigboria O <studysampul@gmail.com>
2023-04-21 11:23:25 +0100

    Refactoring my codes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,8 +34,23 @@ class Base:
             A JSON string of python list object
         """
         if type(list_dictionaries) == list:
-            if list_dictionaries is None or list_dictionaries == []:
+            if list_dictionaries is None:
                 return "[]"
             return json.dumps(list_dictionaries)
         else:
             ...
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Class method that writes a JSON string rep of a list object
+        to a file.
+        Args:
+            list_objs (list): a list of inherited Base Instances
+        """
+        fn = "{}.json".format(cls.__name__)
+        with open(fn, mode="w") as jf:
+            if list_objs is None:
+                jf.write("[]")
+            else:
+                list_obj = [obj.to_dictionary() for obj in list_objs]
+                jf.write(Base.to_json_string(list_obj))
