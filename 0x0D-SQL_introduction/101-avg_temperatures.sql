2242004ce134f269410a42935c00fed00724a4db
Ehigboria Dukeson O <studysampul@gmail.com>
2023-05-16 19:06:49 +0100

    Updates

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,3 @@
 --SQL script that displays the average temperature (Fah) by city order by temp desc
 SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperatures`
-GROUP BY `city`
-ORDER BY `avg_temp` DESC;
+FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
