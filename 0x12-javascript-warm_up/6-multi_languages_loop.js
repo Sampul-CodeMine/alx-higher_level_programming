188bb012bdaa21fcc867acbd6e067d8799c8c30c
Sampul-CodeMine <studysampul@gmail.com>
2023-06-13 15:51:14 +0100

    Solution to Task 4 and Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const languages = ['C', 'Python', 'JavaScript'];
+const mode = ['fun', 'cool', 'amazing'];
+for (let i = 0; i <= 2; i++) {
+  console.log(`${languages[i]} is ${mode[i]}`);
+}
