50ba7c6bc3db818416ddf9e4b38cee0d971f9a81
Sampul-CodeMine <studysampul@gmail.com>
2023-06-14 20:13:09 +0100

    Solutions to Tasks from 100 to 102 added.

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const args = process.argv
+
+const firstFile = fs.readFileSync(args[2]).toString();
+const secondFile = fs.readFileSync(args[3]).toString();
+
+fs.writeFileSync(args[4], firstFile + secondFile);
