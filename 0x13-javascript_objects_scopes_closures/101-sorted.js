fa5205a67b42c15f4971b2743168b684f5992da5
Ehigboria Dukeson O <studysampul@gmail.com>
2023-06-15 10:09:52 -0700

    Updated and completed the code for Task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
+const { dict } = require('./101-data');
 
+const newDict = {};
+
+for (const item in dict) {
+  if (newDict[dict[item]] === undefined) newDict[dict[item]] = [];
+  newDict[dict[item]].push(item);
+}
+console.log(newDict);
