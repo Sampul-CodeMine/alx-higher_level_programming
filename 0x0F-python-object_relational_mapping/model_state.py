aae3f9dbfb0b4ae69809319883e253fbffe5ff5d
Sampul-CodeMine <studysampul@gmail.com>
2023-06-19 13:58:15 +0100

    Added documentation for the model_state file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-
+"""A class that defines State and Base which is an instance of
+the declarative_base()
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
-
-"""A class that defines State and Base which is an instance of
-the declarative_base()
-"""
 
 
 class State(Base):
