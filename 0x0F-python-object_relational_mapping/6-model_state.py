00f8d13c0351b46d6c9b7bbfaec4d40337303ec9
Sampul-CodeMine <studysampul@gmail.com>
2023-06-19 10:14:34 +0100

    Updated codes for Task 6 and the model for 6

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -3,15 +3,15 @@
 """A python file that creates a class that links to a Database
 """
 
-if __name__ == '__main__':
-    import MySQLdb
-    from sys import argv as sysarg
-    from model_state import Base, State
-    from sqlalchemy import create_engine
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+import MySQLdb
 
-    connection = MySQLdb.connect(
-        host="localhost", port=3306, user=sysarg[1],
-        password=sysarg[2], database=sysarg[3])
+if __name__ == '__main__':
+    connection = MySQLdb.connect(host="localhost", port=3306,
+                                 user=sys.argv[1], password=sys.argv[2],
+                                 database=sys.argv[3])
 
     engine = create_engine('mysql+mysqldb://', creator=lambda: connection,
                            pool_pre_ping=True)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,7 +15,7 @@ class State(Base):
     """Definition of the State class
     Inherits from the Base Parent class"""
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
 
     def __init__(self, id):
