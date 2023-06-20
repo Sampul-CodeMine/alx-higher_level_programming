cb78a754df304966efa99ba6a38283e550171d1b
Sampul-CodeMine <studysampul@gmail.com>
2023-06-20 13:26:48 +0100

    Updated the DB definition for Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,7 +12,8 @@ class State(Base):
     """Definition of the State class
     Inherits from the Base Parent class"""
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
     def __init__(self, id, name):
