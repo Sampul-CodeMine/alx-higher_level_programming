63b4a20bb651afe8aee7becddc97200e289e4b95
Dukeson Ehigboria <studysampul@gmail.com>
2023-07-01 09:27:09 +0100

    Solution to Task 7 and Task 8

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes a URL sends a request to the URL and displays the
+body of the response getting the HTTP status code if greater than or equal 400
+"""
+if __name__ == '__main__':
+    import sys.argv as sarg
+    import requests as req
+
+    url = sarg[1]
+    res = req.get(url)
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to a given URL
+with the letter as a parameter
+"""
+if __name__ == '__main__':
+    import sys.argv as sarg
+    import requests as req
+
+    if len(sarg) == 1:
+        letter = ""
+    else:
+        letter = sarg[1]
+    param = {"q": letter}
+    data = req.post('http://0.0.0.0:5000/search_user', data=param)
+    try:
+        res = data.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get('id')
+                                   res.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
